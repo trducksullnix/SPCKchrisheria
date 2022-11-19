5a914f695adf818a822084a74a10661d3b68b627
trducksullnix <111184101+trducksullnix@users.noreply.github.com>
2022-11-19 20:03:00 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+localStorage.setItem("lastname","Tran")
+const danhsachHS = [hs1, hs2];
+console.log(danhsachHS[0])
+localStorage.setItem("name", "Duc");
+localStorage.setItem("age", 16);
+const stringFromLS = localStorage.getItem("name");
+console.log(stringFromLS);
+var numberFromLS = Number(localStorage.getItem("age"));
+console.log(numberFromLS);
